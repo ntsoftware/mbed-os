e0c7399d445642803828a36dc29aa2acab1a7b32
Nicolas Torneri <nicolas.torneri@eolane.com>
2020-03-28 10:11:58 +0100

    copy/paste SiliconLabs EFM32 USB Stack

---
+++ targets/TARGET_Silicon_Labs/TARGET_EFM32/usbconfig.h
@@ -0,0 +1,45 @@
+/***************************************************************************//**
+ * @file
+ * @brief USB protocol stack library, application supplied configuration options.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2018 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#ifndef __USBCONFIG_H
+#define __USBCONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define USB_DEVICE        /* Compile stack for device mode. */
+#define USB_PWRSAVE_MODE (USB_PWRSAVE_MODE_ONSUSPEND | USB_PWRSAVE_MODE_ONVBUSOFF)
+
+/****************************************************************************
+**                                                                         **
+** Specify total number of endpoints used (in addition to EP0).            **
+**                                                                         **
+*****************************************************************************/
+#define NUM_EP_USED 5
+
+/****************************************************************************
+**                                                                         **
+** Specify number of application timers you need.                          **
+**                                                                         **
+*****************************************************************************/
+#define NUM_APP_TIMERS 2
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USBCONFIG_H */
